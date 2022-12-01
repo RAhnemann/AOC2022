ce0ec49aa3d5573a09c04a00af6f88a455a4581e
Rob Ahnemann <rob.ahnemann@gmail.com>
2022-12-01 00:10:48 -0600

    Day1

--- Source/Day01/Solution.cs
+++ Source/Day01/Solution.cs
@@ -1,18 +1,64 @@
-﻿namespace AdventOfCode.Day01;
+﻿using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day01;
 
 public class Solution : BaseSolution
 {
-    public Solution() : base(1, "")
+    public Solution() : base(1, "Hungry Elves")
     {
     }
 
     public override string GetPart1Answer()
     {
-        return string.Empty;
+        List<string> lines = File.ReadAllLines("D:\\Dev\\aoc2022\\Source\\Input\\Day1.txt").ToList();
+
+        int maxSum = 0;
+            
+        int runTotal = 0;
+
+        foreach(string line in lines)
+        {
+            if(string.IsNullOrEmpty(line))
+            {
+                if (runTotal > maxSum)
+                    maxSum = runTotal;
+
+                runTotal = 0;
+            }
+            else
+            {
+                runTotal += int.Parse(line);
+            }
+        }    
+
+        return maxSum.ToString();
     }
 
     public override string GetPart2Answer()
     {
-        return string.Empty;
+        List<string> lines = File.ReadAllLines("D:\\Dev\\aoc2022\\Source\\Input\\Day1.txt").ToList();
+
+        List<int> sums = new List<int>();
+
+        int runTotal = 0;
+
+        foreach (string line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                sums.Add(runTotal);
+
+                runTotal = 0;
+            }
+            else
+            {
+                runTotal += int.Parse(line);
+            }
+        }
+
+        sums.Sort();
+        sums.Reverse();
+        return (sums[0] + sums[1] + sums[2]).ToString();
     }
 }
