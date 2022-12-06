1ffb5e5fd9ef1d8d68e606d53eed1c6e3df72dc1
Rob Ahnemann <rob.ahnemann@gmail.com>
2022-12-05 23:18:25 -0600

    Day 6

--- Source/Day06/Solution.cs
+++ Source/Day06/Solution.cs
@@ -1,18 +1,41 @@
-﻿namespace AdventOfCode.Day06;
+﻿using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day06;
 
 public class Solution : BaseSolution
 {
-    public Solution() : base(6, "")
+    public Solution() : base(6, "Coded Marker")
     {
     }
 
     public override string GetPart1Answer()
     {
-        return string.Empty;
+        return FindMarker(File.ReadAllLines("D:\\Dev\\aoc2022\\Source\\Input\\Day6.txt")[0], 4);
     }
 
     public override string GetPart2Answer()
     {
-        return string.Empty;
+          return FindMarker(File.ReadAllLines("D:\\Dev\\aoc2022\\Source\\Input\\Day6.txt")[0], 14);
     }
+
+    public string FindMarker(string input, int charsNeeded)
+    {
+        int marker = 0;
+
+        while (!IsUnique(input.Substring(marker, charsNeeded)))
+        {
+            marker++;
+        }
+
+        marker += charsNeeded;
+
+        return marker.ToString();
+    }
+
+    public bool IsUnique(string input)
+    {
+        return input.Distinct().Count() == input.Length;
+    }
+
 }
